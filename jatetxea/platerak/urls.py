fdbb428344c954b553b213a494ba319152bfb6ad
xabier <diaz.xabier@uni.eus>
2022-10-04 13:42:54 +0200

    add, delete egunda (update oraindik ez)

--- jatetxea/platerak/urls.py
+++ jatetxea/platerak/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),  #dirige el add.html a la funcion addrecord de views
+    path('delete/<int:id>', views.delete, name='delete'),
 ]
