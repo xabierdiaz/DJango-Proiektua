5a9b0b6d1eb21bae1a3c5ffeec85db7fa03fa0d3
xabier <diaz.xabier@uni.eus>
2022-10-11 13:22:22 +0200

    Ariketa bukatuta

--- jatetxea/platerak/models.py
+++ jatetxea/platerak/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+from django.db import models
+from langileak.models import Sukaldaria
+
 # Create your models here.
 class Platera(models.Model):
     platera = models.CharField(max_length=50)
     kopurua = models.IntegerField()
+    sukaldaria = models.ForeignKey(Sukaldaria, on_delete=models.CASCADE, null=True)
+    def __str__(self): # objetuari deitzerakoan self.*** jartzen dugunari deitzen diogu
+        return u'%s'%self.platera
